7fa927161ebb8f3de52e371effaddfb6cebe8319
SiAstRoTEK <165885287+SiAstRoTEK@users.noreply.github.com>
2024-04-22 12:30:45 +0200

    NewProjectAdded

---
+++ APIRestUsers/src/main/java/com/ProjectAPI/APIRestUsers/controller/UserREST.java
@@ -0,0 +1,48 @@
+package com.ProjectAPI.APIRestUsers.controller;
+
+import com.ProjectAPI.APIRestUsers.entity.User;
+import com.ProjectAPI.APIRestUsers.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/api/userlist")
+public class UserREST {
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody User user){
+        User newUser = service.addUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") Long id){
+        User foundUser = service.getUser(id);
+        return new ResponseEntity<>(foundUser, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> allUsers = service.getAllUser();
+        return new ResponseEntity<>(allUsers, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public  ResponseEntity<User> updateUser(@PathVariable("id") Long id, @RequestBody User user){
+        User updatedUser = service.updateUser(id, user);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable("id") Long id){
+        service.deleteUser(id);
+        return ResponseEntity.ok("The User have been correctly eliminated!");
+    }
+}
---
+++ APIRestUsers/src/main/java/com/ProjectAPI/APIRestUsers/entity/User.java
@@ -0,0 +1,35 @@
+package com.ProjectAPI.APIRestUsers.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Table(name = "UserList")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name ="Name", nullable = false)
+    private String name;
+
+    @Column(name ="Surname", nullable = false)
+    private String surname;
+
+    @Column(name ="Username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name ="Password", nullable = false)
+    private String password;
+
+    @Column(name ="Email", nullable = false, unique = true)
+    private String email;
+}
